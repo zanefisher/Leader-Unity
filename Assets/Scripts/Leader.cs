08728ce281cffa0713077f051581b36967867d2c
BoomYan <jiamingyan@hotmail.com>
2015-03-25 14:51:46 -0400

    leader.cs world.cs

--- Assets/Scripts/Leader.cs
+++ Assets/Scripts/Leader.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Leader : MonoBehaviour {
-
+	float speed=5f;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +10,10 @@ public class Leader : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		targetPos = new Vector3(targetPos.x, targetPos.y, 0);
+		if(targetPos != transform.position)
+			transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
 	}
 }
