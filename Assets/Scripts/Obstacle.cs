e0d3a1a8eee2c8fe37b2d6aa0e26afc1a554db0f
a56097634a <a56097634a@gmail.com>
2015-03-25 13:54:36 -0400

    vanish obstacles

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -8,13 +8,40 @@ public class Obstacle : MonoBehaviour {
 	public static float obstacleMaxRadius = 200;
 	public static int obstacleSpawnPeriodMin = (int) (60000000 / (float) Controller.screenSize); //60; 
 	public static int obstacleSpawnPeriodMax = (int) (300000000 / (float) Controller.screenSize);//300;
+	private float lifePoint=2f;
+	private bool isMoving=true;
+	public GameObject World;
+	//private bool isEntrance=false;
 	// Use this for initialization
 	void Start () {
-	
+		lifePoint = transform.localScale.x;
+		//should have set the moving bool to indicate the
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		vanish ();
 	}
+
+	void vanish(){
+		if (lifePoint <= 0) {
+			DestroyObject(this);
+		}
+		if ((transform.position - World.transform.position).magnitude > 30f) {
+			DestroyObject(this);
+		}
+	}
+
+	void onTriggerEnter2D(Collision2D coll){
+		if (coll.gameObject.name == "Swamling") {
+			lifePoint-=0.5f;
+			//DestroyObject(coll);
+		}
+	}
+
+	void showEntrance(){
+		if (isMoving) {
+		}
+	}
+	
 }
